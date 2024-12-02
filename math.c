5caac880cc60ec5883f5c0f6f0d2747dea34981b
acari <37274132+acaridotme@users.noreply.github.com>
2024-12-02 09:03:26 +0100

    consegna

---
+++ math.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include potenze.h
+
+int addizione(int operando,int operando){
+return int operando +int operando; 
+}
+int sottrazione(int minuendo,int sottraendo){
+    return int minuendo-int sottraendo;
+    int moltiplicazione(int valore,int valore){
+        return int valore *int valore;
+
+
+    }
+int divisione(float divisore,float dividendo){
+    return float divisore/float dividendo;
+}
+
+}
+
+
+
+
+
---
+++ math.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int A,B;
+int somma,risultato;
+int main(){
+    printf("inserisci il primo valore");
+    scanf("%d,&A");
+    printf("inserisci il secondo valore");
+    scanf("%d,&B");
+    somma=%d+%d=%d,A,B,risultato;
+    printf("la somma di %d e %d è %d\n"A,B,risultato);
+return=0
+}
+
+
+
+
+
